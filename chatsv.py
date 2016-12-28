3dc18b8c84c59106a5a7041624d67a6d268d0fa1
Matheus Vitório <mvfc@users.noreply.github.com>
2016-12-28 11:07:18 -0200

    Update chatsv.py

--- chatsv.py
+++ chatsv.py
@@ -11,19 +11,19 @@ def server():
 	except socket.error:
 		sys.exit()
 	sock1.bind((HOST, PORT))
-	resposta = 'Mensagem recebida com sucesso.'
+	answer = 'Message received with success'
 
 	while True:
 		data, addr = sock1.recvfrom(1024)
-		print('Mensagem recebida.')
+		print('Message received')
 		print(data)
 		try:
 			sock2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 		except socket.error:
 			sys.exit()
 		time.sleep(2)
-		sock2.sendto(resposta.encode('utf-8'), (HOST, PORT2))
-		print('Resposta enviada')
+		sock2.sendto(answer.encode('utf-8'), (HOST, PORT2))
+		print('Answer sent')
 		sock2.close()
 
 server()
